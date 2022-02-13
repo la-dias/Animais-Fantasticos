2ff3c779d0eecd7e23b424c4e3af59a8be2ad925
Lucas Dias <lucas.av.dias1991@gmail.com>
2022-02-13 16:22:25 -0300

    Seleção DOM 101 a 168

--- script.js
+++ script.js
@@ -96,11 +96,73 @@ function showElement(evento) {
 
 todosElementos.forEach((elemento) => elemento.addEventListener('click', showElement))
 
-
-
 */
 
+///////////////////////////////////
 /* SELEÇÃO DE ELEMENTOS PELO DOM */
+///////////////////////////////////
 
-const animais = document.getElementById('animais');
-console.log(animais);
+//const animais = document.getElementById('animais');
+//console.log(animais);
+
+//OBS: getElementsByClassName e getElementsByTagName a lista roda ao Vivo. Se houver alteração, ela será atualizada.
+
+//const gridSection = document.getElementsByClassName('grid-section');
+//console.log(gridSection[1]);
+
+//const primeiraLi = document.querySelector('li');
+//console.log(primeiraLi);
+
+//const primeiraUl = document.querySelector('ul');
+//console.log(primeiraUl);
+
+//const linkInterno = document.querySelector('[href^="#"]');
+//console.log(linkInterno);
+
+//OBS: QuerySelectorAll trás uma nodelist, que é arraylike, porém estática; Ela não se atualiza.
+
+//const animaisImg = document.querySelectorAll('.animais img');
+//console.log(animaisImg[0]);
+
+//const titulo = document.querySelector('.titulo');
+//const gridSectionHTML = document.getElementsByClassName('grid-section');
+//const gridSectionNode = document.querySelectorAll('.grid-section');
+
+//titulo.classList.add('grid-section');
+
+//console.log(gridSectionHTML);
+//console.log(gridSectionNode);
+
+//Array-Like são parecidas com Arrays, mas não são Arrays.
+
+//const gridSection = document.querySelectorAll('.grid-section');
+
+//gridSection.forEach(function (gridItem, index, array) {
+//gridItem.classList.add('azul');
+//console.log(index);
+//console.log(array);
+//});
+
+//Exercícios DOM//
+
+/*
+// Retorne no console todas as imagens do site
+const imagens = document.querySelectorAll('img');
+console.log(imagens);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensImg = document.querySelectorAll('[src^="img/imagem"]');
+console.log(imagensImg);
+
+// Selecione todos os links internos (onde o href começa com #)
+const linksInternoss = document.querySelectorAll('[href^="#"]');
+console.log(linksInternoss);
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const oPrimeiroH2 = document.querySelector('.animais-descricao h2');
+console.log(oPrimeiroH2);
+
+// Selecione o último p do site
+const oUltimoP = document.getElementsByTagName('p');
+console.log(oUltimoP[oUltimoP.length - 1]);
+*/
